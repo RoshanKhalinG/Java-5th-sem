8371f97625c69d640437e937237cdd15a95875e5
RoshanKhalinG <102815285+RoshanKhalinG@users.noreply.github.com>
2024-01-05 21:41:03 +0545

    TypeCasting and Coercion

---
+++ 5App.js
@@ -0,0 +1,76 @@
+/*
+// Type Casting and Type Coercion
+let a = 1
+let b = '2'
+
+let c = b + a
+let c = parseInt(b) + a
+console.log(c , typeof c);
+
+c = b - a
+console.log(c , typeof c);
+
+c = b*a
+console.log(c , typeof c);
+
+c = b/a
+console.log(c , typeof c);
+*/
+
+/*
+let d = Number('2')
+console.log(d+2);
+
+let e = parseInt('2')
+console.log(e+2);
+
+let f = Number(true)
+let g = Number(false)
+
+console.log(f)
+console.log(g);
+*/
+
+/*
+//----To convert number to string------//
+let h= 12
+
+let i = 12 + ''
+let j = String(h)
+let k = toString(h)
+
+console.log(i , typeof i);
+console.log(j , typeof j);
+console.log(k , typeof k);
+
+
+let l = Boolean(0)
+console.log(l);
+
+let m= Boolean(1)
+console.log();
+
+let n = Boolean('mn')
+console.log(n);
+
+let o = Boolean()
+console.log(o);
+*/
+
+/*
+//-------NaN------//
+let a = "Hello"
+let b = 2
+console.log(a/2);
+console.log(typeof NaN);
+
+
+console.log(isNaN(2));
+
+
+let d = '2a'
+let e = parseInt(d)
+let f = Number('2a')
+console.log(e , f);
+*/
+
