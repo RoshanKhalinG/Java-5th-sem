c319c97c2d58dc0dbb68307d652e8bec0100212b
RoshanKhalinG <102815285+RoshanKhalinG@users.noreply.github.com>
2024-01-05 21:47:40 +0545

    Array

---
+++ 7App.js
@@ -0,0 +1,85 @@
+
+/*
+// ----------------Array----------------//
+
+ let fruits = ['mango' , 'apple' , 'orrange' ]
+
+ let a = [1 , 'a' , 2, 'b' , ['a' , 'b']]
+
+ //-----------Getting Element From  An Array------------// 
+
+  let c = a[2]
+  console.log(c);
+  
+ //-----------Getting Length of an Array------------//  
+  let d = a.length
+  console.log(d);
+
+  a.push ('zzz')
+  console.log(a);
+
+
+//------Removing element from an array---------//
+
+ a.pop()
+ console.log(a);
+
+
+
+ //--------------Looping-----------------//
+
+ //-----For Loop :
+
+//  for (let i = 0; i <= 5; i++) {
+//     console.log(i); 
+//  }
+
+
+ // -----Looping through a array :
+ let a = [ 1,2,3]
+ for (let i = 0; i <= 5; i++) {
+    console.log(a[i]); 
+ }
+
+ 
+
+ //---------------Coding Challenge------------------//
+
+ function name(params) {
+   
+ }*/
+
+
+//  setTimeout(
+//    function () {
+//       console.log('inside');
+//    },3000
+   
+// console.log('outside');
+
+//-------2--------//
+/*
+function filterEvenNumbers(array) {
+   let new_arr = [];
+   for (let i = 0; i < array.length; i++) {
+      if ( array[i] % 2 === 0){
+      new_arr.push(array[i]);
+      }
+   }
+   return new_arr;
+}
+
+let d = [1, 2, 3, 4, 5, 6 , 'sss'];
+let e = filterEvenNumbers(d);
+console.log(e);
+*/
+
+
+// let fname = 'Roshan'
+// let lname = 'Rai'
+
+// c = fname  + lname;
+
+// document.write(c);
+// console.log(c);
+
