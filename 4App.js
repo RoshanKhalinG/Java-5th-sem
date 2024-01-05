bde719189da7ac5336b2a5d7d19931073591c554
RoshanKhalinG <102815285+RoshanKhalinG@users.noreply.github.com>
2024-01-05 21:36:02 +0545

    aa

---
+++ 4App.js
@@ -0,0 +1,145 @@
+
+//-----------------perator-----------------//
+
+/*
+// 1. Assignment Operator (= , += , -= , /=, *=)
+
+let a=2
+a += 2 // a=a+2
+console.log(a);
+
+a -= 2 // a +a-2
+console.log(a);
+
+a/=2 // a=a/2
+console.log(a);
+
+a*=2 // a=a*2
+console.log(a);
+
+
+
+// 2.Addtion Operator :
+
+   let a = 2
+   a = a+2
+
+// 3.Subtraction Operator (-) :
+   a = a-2
+
+// 4.Multiplication Operator (*) :
+   a = a*2
+// 5.Division Operator (/) :
+   a = a/2
+
+// 6.Remainder Operator (%) :
+    let b = 3%2
+    console.log(b);
+
+ // 7.Increment Operator (++) :  
+    let c = 7
+    c++
+    console.log(c); 
+
+ // 8.Decrement Operator (--) : 
+    c--
+    console.log(c);
+
+// Exponentian (**) :
+
+    let d = 3 ** 2 // 3 to the power 2
+    console.log(d);
+
+*/
+
+
+/*
+//Comparision Operator (== , === , != , > , >=, <=) :
+
+  const a =2
+  const b =2
+  const c =3
+  const d = "2"
+
+  console.log( a==b);//true
+  console.log(a==d);//true(equals)
+  console.log(a===d);//false(strictly equal) compare type as well
+  console.log(a!=d);// false(not equals)
+  console.log(a!==d);//True(strictlt not equal)
+  console.log(c>b);//true (greater)
+  console.log(c<d);//false(less than)
+  console.log(c>=a);//true gtreter than or equal to
+  console.log(c<=a);//false less than or euqal to
+  console.log(b>=a);//true
+  console.log(d>c);//true
+*/
+
+
+/*
+ // Logical Operator (AND OR NOT):
+
+   const x = 6
+   const y = 3
+  //AND
+   console.log(x<10 && y>1); // true
+   console.log(x<10 && y < 1);// false
+  //OR
+   console.log(x==5 || y==5);//false
+   console.log(x==6 || y==0);//true 
+  //NOT
+  console.log(!(x===y));//True 
+  console.log(!(x>y))//false
+*/
+
+
+/*
+// Conditional (Ternary) Operator  :
+  
+   const age = 19
+   const age1 = 12
+
+    const votable = (age < 18)? "Too Young" : "Old enough"
+    console.log(votable);
+
+    const votable = (age1 < 18)? "Too Young" : "Old enough"
+    console.log(votable);
+*/
+
+
+/*
+//-----Coding Challenge--------//
+
+// 1. Multiply 20 by 4 and save into the console
+     
+let a = 20*4
+console.log(a);
+
+// 2.Declare a constant variable and assign it the value of 5 to the power 4 and log it into the console
+     const b = 5 ** 4
+     console.log(b);
+
+// 3. DEclare a constant and assign  it a remainder of dividing 83 by 6 and log it into console
+     const c = 83 % 6
+     console.log(c);    
+
+// 4. Declare a varable and assign a value 4 using increment operator increage the value by 1 and log it into console
+      let d = 4
+      d++
+      console.log(d);
+
+
+  // 2. Declare a variable day and assign Sunday to that variable . If the day is monday log to the console my luckey day otherwise log to the console my worst days
+       let day = 'Sunday'
+       console.log((day === 'Monaday'? 'My Luckey Day' : 'My Worst Day'));
+        let check = (day == 'Monday' ? 'Luckey Day' : 'Worst Day')
+        console.log(check);
+
+
+  // 3. Declare three variable one , two and three assign 1 to one and 2 to one and 2 to two and 
+        
+  let one = 1
+  let two = 2
+  let three =4
+
+  console.log(three>2 && three > one?'Hurray':'Bad Luck');
+*/
